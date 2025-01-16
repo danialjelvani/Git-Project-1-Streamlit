2072dcc977e6a1cd56ebf95c92d2162911f2ce5f
Danial Jelvani <jelvanidanial@gmail.com>
2025-01-16 22:20:23 +0330

    streamlit 1

---
+++ run.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+# in terminal type : streamlit run run.py
+# this runs it as a website
+
+st.title('Become Your Best at Dr Jelvani Aesthetic Clinic 😎')
+
+# emoji cheat sheet for using emojis
